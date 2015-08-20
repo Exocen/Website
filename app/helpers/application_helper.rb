7993e9ef84c134704817237de2dd4a93fef353cc
Exocen <exo@localhost.localdomain>
2015-08-20 19:32:49 +0200

    change title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #return the full title
   def full_title(page_title = '')
-    base_title = "Titratrouver"
+    base_title = "4MyKitten"
     if page_title.empty?
       base_title
     else
