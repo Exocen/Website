4cdaf4d03b50494cf3e6982cce932583fd9f6eb5
Exocen <exo@localhost.localdomain>
2015-08-20 23:51:08 +0200

    debut de function

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,15 @@
 module StaticPagesHelper
+
+	#test image
+	def getCarouselExampleGeneric(slideNumber)
+		li = ''
+		$i = 0
+		while $i < slideNumber do
+			li +='<li data-target="#carousel-example-generic"
+			data-slide-to="'+$i.to_s+'" class=""></li>'
+			$i += 1;
+		end
+		return li.html_safe
+	end
+
 end
