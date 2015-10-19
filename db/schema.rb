313856f4df25249a17f8443ae593f19693da76e2
exo <exo@localhost.localdomain>
2015-10-19 14:40:01 +0200

    add chat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906143136) do
+ActiveRecord::Schema.define(version: 20151019120335) do
+
+  create_table "chats", force: :cascade do |t|
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "link"
