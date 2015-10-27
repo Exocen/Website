6affc771fa3d37e283e6165fdff8938fd6157e9f
Exocen <exo@vps101929.ovh.net>
2015-10-27 17:08:34 +0100

    mod taille image

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ActiveRecord::Base
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "800x800" }
   validates_attachment_presence  :image
   validates_attachment_content_type :image, presence: true, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
