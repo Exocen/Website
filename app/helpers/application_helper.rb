aa14148ff8d314dddfffb02f9e363df1e0a64a15
Exocen <Exocen@users.noreply.github.com>
2016-03-08 00:09:19 +0100

    cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-
-  #return the full title
-  def full_title(page_title = '')
-    base_title = "4MyKitten"
-    if page_title.empty?
-      base_title
-    else
-      "#{page_title} | #{base_title}"
-    end
-  end
-
 end
--- app/helpers/videos_helper.rb
+++
@@ -1,9 +0,0 @@
-module VideosHelper
-  #return videos
-  def getLinkedVideo(video)
-    $videolink = video.link
-    li ='<iframe width="560" height="315" src=' + $videolink + ' allowfullscreen></iframe>'
-    return li.html_safe
-  end
-
-end
