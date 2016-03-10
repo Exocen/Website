1a00872395f1a8de87955ce918a35b88938317a5
Exocen <Exocen@users.noreply.github.com>
2016-03-10 21:04:17 +0100

    debug 15

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -9,28 +9,19 @@ function GoCarrousel()
     arrows: false
 
   });
-    $('.slick-list').css( 'height', 'auto');
+  $('.slick-list').css( 'height', 'auto');
 }
 function GoDropzone(){
-	// disable auto discover
-	Dropzone.autoDiscover = false;
-
-	// grap our upload form by its id
-	$("#new_photo").dropzone({
-		// restrict image size to a maximum 1MB
-		maxFilesize: 1,
-		// changed the passed param to one accepted by
-		// our rails app
-		paramName: "photo[image]",
-		success: function(file, response){
-			// find the remove button link of the uploaded file and give it an id
-			// based of the fileID response from the server
-			$(file.previewTemplate).find('.dz-remove').attr('id', response.fileID);
-			// add the dz-success class (the green tick sign)
-			$(file.previewElement).addClass("dz-success");
-		}
-	});
+  // Dropzone.autoDiscover = false;
+  $("#new_photo").dropzone({
+    maxFilesize: 1,
+    paramName: "photo[image]",
+    success: function(file, response){
+      $(file.previewTemplate).find('.dz-remove').attr('id', response.fileID);
+      $(file.previewElement).addClass("dz-success");
+    }
+  });
 }
-$(function () {
-	$('[data-toggle="tooltip"]').tooltip()
-})
+function () {
+  $('[data-toggle="tooltip"]').tooltip()
+}
