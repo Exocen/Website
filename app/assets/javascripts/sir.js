4e66e183b24f6c50e8abfd4593afac3609f3219f
qbailleul <quentin.bailleul@atos.net>
2016-03-11 15:14:43 +0100

    debug 4

--- app/assets/javascripts/sir.js
+++ app/assets/javascripts/sir.js
@@ -75,13 +75,7 @@ function stateChanges() {
   }
 }
 
-function infection() {
-  if (!rerun) transmissionRate = 0.35;
-  else {
-    transmissionRate = 1;
-  }
-
-  var numberOfInfectionsPerRound = 0;
+function numberOfInfectionsPerRoundManage(numberOfInfectionsPerRound){
   for (var index = 0; index < graph.nodes.length; index++) {
     if (graph.nodes[index].status !== "S") continue;
     var susceptible = graph.nodes[index];
@@ -103,6 +97,18 @@ function infection() {
       exposeIndividual(susceptible, exposer);
     }
   }
+  return numberOfInfectionsPerRound;
+}
+
+function infection() {
+  if (!rerun) transmissionRate = 0.35;
+  else {
+    transmissionRate = 1;
+  }
+
+  var numberOfInfectionsPerRound = 0;
+
+  numberOfInfectionsPerRound = numberOfInfectionsPerRoundManage(numberOfInfectionsPerRound);
 
   if (numberOfInfectionsPerRound > 0) {
     rerun = false;
@@ -126,82 +132,61 @@ function infection() {
 
 function infection_noGuaranteedTransmission() {
   var numberOfInfectionsPerRound = 0;
-  for (var index = 0; index < graph.nodes.length; index++) {
-    if (graph.nodes[index].status !== "S") continue;
-    var susceptible = graph.nodes[index];
-    var neighbors = findNeighbors(susceptible);
-    var infectedNeighborArray = [];
-    var numberOfInfectedNeighbors = 0;
-    for (var neighborIndex = 0; neighborIndex < neighbors.length; neighborIndex++) {
-      var neighbor = neighbors[neighborIndex];
-      if (neighbor.status === "I") {
-        infectedNeighborArray.push(neighbors[neighborIndex]);
-        numberOfInfectedNeighbors++;
+  numberOfInfectionsPerRound = numberOfInfectionsPerRoundManage(numberOfInfectionsPerRound);
+
+  function getStatuses(infectedClass) {
+    var S = 0;
+    var I = 0;
+    var R = 0;
+    var V = 0;
+
+    for (var index = 0; index < graph.nodes.length; index++) {
+      var individual = graph.nodes[index];
+
+      if (individual.status === "S") S++;
+      if (individual.status === "I") I++;
+      if (individual.status === "R") R++;
+      if (individual.status === "V") V++;
+    }
+
+    if (infectedClass === "S") return S;
+    if (infectedClass === "I") return I;
+    if (infectedClass === "R") return R;
+    if (infectedClass === "V") return V;
+
+  }
+
+
+
+  function detectEndGame() {
+    updateCommunities();
+    var numberOf_AtRisk_communities = 0;
+    for (var groupIndex = 1; groupIndex < numberOfCommunities+1; groupIndex++) {
+      var numberOfSusceptiblesPerGroup = 0;
+      var numberOfInfectedPerGroup = 0;
+
+      for (var nodeIndex = 0; nodeIndex < graph.nodes.length; nodeIndex++) {
+        var node = graph.nodes[nodeIndex];
+        if (parseFloat(node.group) === groupIndex){
+          if (node.status === "S") numberOfSusceptiblesPerGroup++;
+          if (node.status === "I") numberOfInfectedPerGroup++;
+          if (node.status === "E") numberOfInfectedPerGroup++;
+        }
+      }
+      if (numberOfInfectedPerGroup > 0) {
+        if (numberOfSusceptiblesPerGroup > 0) {
+          numberOf_AtRisk_communities++;
+        }
       }
     }
-    var probabilityOfInfection = 1.0 - Math.pow(1.0 - transmissionRate,numberOfInfectedNeighbors);
-    if (Math.random() < probabilityOfInfection) {
-      numberOfInfectionsPerRound++;
-      var shuffledInfectedNeighborArray = shuffle(infectedNeighborArray);
-      var exposer = shuffledInfectedNeighborArray[0];
-      exposeIndividual(susceptible, exposer);
-    }
-  }
-}
-
-function getStatuses(infectedClass) {
-  var S = 0;
-  var I = 0;
-  var R = 0;
-  var V = 0;
-
-  for (var index = 0; index < graph.nodes.length; index++) {
-    var individual = graph.nodes[index];
-
-    if (individual.status === "S") S++;
-    if (individual.status === "I") I++;
-    if (individual.status === "R") R++;
-    if (individual.status === "V") V++;
-  }
-
-  if (infectedClass === "S") return S;
-  if (infectedClass === "I") return I;
-  if (infectedClass === "R") return R;
-  if (infectedClass === "V") return V;
-
-}
-
-
-
-function detectEndGame() {
-  updateCommunities();
-  var numberOf_AtRisk_communities = 0;
-  for (var groupIndex = 1; groupIndex < numberOfCommunities+1; groupIndex++) {
-    var numberOfSusceptiblesPerGroup = 0;
-    var numberOfInfectedPerGroup = 0;
-
-    for (var nodeIndex = 0; nodeIndex < graph.nodes.length; nodeIndex++) {
-      var node = graph.nodes[nodeIndex];
-      if (parseFloat(node.group) === groupIndex){
-        if (node.status === "S") numberOfSusceptiblesPerGroup++;
-        if (node.status === "I") numberOfInfectedPerGroup++;
-        if (node.status === "E") numberOfInfectedPerGroup++;
+    if (numberOf_AtRisk_communities === 0 && diseaseIsSpreading) {
+      endGame = true;
+      diseaseIsSpreading = false;
+      timeToStop = true;
+      if (vaccinateMode && !quarantineMode && !game) {
+        animatePathogens_thenUpdate();
+        tutorialUpdate();
       }
-    }
-    if (numberOfInfectedPerGroup > 0) {
-      if (numberOfSusceptiblesPerGroup > 0) {
-        numberOf_AtRisk_communities++;
-      }
-    }
-  }
-  if (numberOf_AtRisk_communities === 0 && diseaseIsSpreading) {
-    endGame = true;
-    diseaseIsSpreading = false;
-    timeToStop = true;
-    if (vaccinateMode && !quarantineMode && !game) {
-      animatePathogens_thenUpdate();
-      tutorialUpdate();
-    }
 
+    }
   }
-}
