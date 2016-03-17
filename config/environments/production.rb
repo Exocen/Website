349aaa84a14b704a3d94add2ec36ecf5a8dbc283
Exocen <Exocen@users.noreply.github.com>
2016-03-17 19:03:47 +0100

    add secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,13 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :yui
 
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = false
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile =  ['*.js', 'custom.css', '*.css.erb'] 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
