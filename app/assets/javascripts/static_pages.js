9e88795ae2adfa4d9f35f0ee272d63b59765588f
qbailleul <quentin.bailleul@atos.net>
2016-03-23 10:25:59 +0100

    debug clock js

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,14 +1,13 @@
 //= require flipclock.min
-function CountDownTimer(dt)
-{
-  var currentDate = new Date();
-  var futureDate  = new Date(dt);
-  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-  $(document).ready(function() {var clock = $('.clock').FlipClock(diff,{
-    clockFace: 'DailyCounter',
-    showSeconds: false,
-    autoStart: true,
-    defaultLanguage: 'french'
-  });
-}
+var currentDate = new Date();
+var futureDate  = new Date(dt);
+var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+$(document).ready(function() {var clock = $('.clock').FlipClock(diff,{
+  clockFace: 'DailyCounter',
+  showSeconds: false,
+  autoStart: true,
+  defaultLanguage: 'french'
+});
+});
