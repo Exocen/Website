d292e053551923db5a2941f48d32b727cf8900e5
qbailleul <quentin.bailleul@atos.net>
2016-03-23 10:52:05 +0100

    last clock correction

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,13 +1,15 @@
 //= require flipclock.min
+function CountDownTimer(dt)
+{
+  var currentDate = new Date();
+  var futureDate  = new Date(dt);
+  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-var currentDate = new Date();
-var futureDate  = new Date(dt);
-var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-
-$(document).ready(function() {var clock = $('.clock').FlipClock(diff,{
-  clockFace: 'DailyCounter',
-  showSeconds: false,
-  autoStart: true,
-  defaultLanguage: 'french'
+  $(document).ready(function() {var clock = $('.clock').FlipClock(diff,{
+    clockFace: 'DailyCounter',
+    showSeconds: false,
+    autoStart: true,
+    defaultLanguage: 'french'
+  });
 });
-});
+}
