593e016f6b5e78f7324742912333f3c5c1713b02
qbailleul <quentin.bailleul@atos.net>
2016-03-24 16:34:57 +0100

    change connection redirection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user
-      redirect_to user
+      redirect_to root
     else
       flash[:danger] = 'Invalid name/password combination'
       render 'new'
