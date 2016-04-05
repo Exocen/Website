79301416f0f164462e318edfbe5419af7a2d8bfb
Exocen <Exocen@users.noreply.github.com>
2016-04-05 16:05:16 +0200

    Update video.rb

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,4 @@
 class Video < ActiveRecord::Base
-validates :link, presence: true, uniqueness: { case_sensitive: true }, format { with: \A((https:\/\/www\.youtube\.com\/watch\?v=)+[a-zA-Z0-9]*)\z/i, on: :create }
+validates :link, presence: true, uniqueness: { case_sensitive: true }, format { with: \A((https:\/\/www\.youtube\.com\/watch\?v=)+[a-zA-Z0-9]*)\z/i }
   belongs_to :user
 end
