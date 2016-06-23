7704e9417c09f982d0f6ae4dc5757ffc06eb269a
Exocen <Exocen@users.noreply.github.com>
2016-06-23 14:21:12 +0200

    test netrics

--- app/assets/javascripts/netrics.js
+++ app/assets/javascripts/netrics.js
@@ -827,15 +827,19 @@ function getAttribute(obj){
 
 }
 
-function  getCxCy(obj, w, h){
+function getCxCy(obj, w, h){
   return obj.attr("cx", function(d) {
-    d.x = Math.max(8, Math.min(w, d.x));
+    d.x = mathMax(w, d.x);
     return d.x;
   })
   .attr("cy", function(d) {
-    d.y = Math.max(8, Math.min(h, d.y));
+    d.y = mathMax(h, d.y);
     return d.y;
   });
+}
+
+function mathMax(x, y){
+  return Math.max(8, Math.min(x, y));
 }
 
 function getFriction(nod, link, charg, fric, tic){
